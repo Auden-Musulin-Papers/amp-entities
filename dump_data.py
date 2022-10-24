d4d5230659da046d6f9869bff536c46072e7814b
csae8092 <peter.andorfer@oeaw.ac.at>
2022-10-24 11:59:26 +0200

    indentation set to 2

--- dump_data.py
+++ dump_data.py
@@ -1,4 +1,4 @@
 from config import br_client, BASEROW_DB_ID
 
-files = br_client.dump_tables_as_json(BASEROW_DB_ID, folder_name="json_dumps")
+files = br_client.dump_tables_as_json(BASEROW_DB_ID, folder_name="json_dumps", indent=2)
 print(files)
